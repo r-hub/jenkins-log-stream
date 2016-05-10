7fb2639786f46b664b95091e59e2b61325fe1834
Gabor Csardi <csardi.gabor@gmail.com>
2016-05-10 10:20:08 +0100

    First working version

---
+++ README.md
@@ -0,0 +1,38 @@
+
+# jenkins-log-stream
+
+> Stream the output of a Jenkins build
+
+## Install
+
+```
+$ npm install --save jenkins-log-stream
+```
+
+## Usage
+
+```js
+var JenkinsLogStream = require('jenkins-log-sream');
+var stream = new JenkinsLogStream({
+  'baseUrl': 'http://user:pass@jenkins.server:8080',
+  'job': 'jenkins-job',
+  'build': 'lastBuild',
+  'pollInterval': 1000
+});
+
+stream.pipe(process.stdout);
+```
+
+## Options
+
+* `baseUrl` The Jenkins base URL, including the username and password, and
+  also the port if it is not 80. Required.
+* `job` The name of the Jenkins job. Required.
+* `build` The id of the build, a numeric value or `'lastBuild'` for the last build.
+  Defaults to `'lastBuild'`.
+* `pollInterval` Integer to specify how often to poll the Jenkins server, in
+  milliseconds. Defaults to 1000, i.e. one second.
+
+## License
+
+MIT @ [R consortium](https://github.com/r-hub)
--- index.js
+++ index.js
@@ -1,6 +1,62 @@
 
-function make_stream() {
+const Readable = require('stream').Readable;
+const util = require('util');
+const got = require('got');
+const url = require('url');
 
+util.inherits(JenkinsOutput, Readable);
+
+function JenkinsOutput(options) {
+    Readable.call(this, options);
+    this._baseUrl = options.baseUrl;
+    this._job = options.job;
+    this._build = options.build || 'lastBuild';
+    this._url = url.parse(
+	this._baseUrl + '/job/' + this._job + '/' + this._build +
+	    '/logText/progressiveText'
+    );
+    this._n = 0;
+    this._pollInterval = options.pollInterval || 1000;
 }
 
-module.exports = make_stream;
+JenkinsOutput.prototype._read = function() {
+    var self = this;
+    if (this._done) { self.push(null); }
+
+    function get() {
+
+	var url = self._url.protocol + '//' + self._url.host +
+	    self._url.path;
+
+	got.post(url, {
+	    auth: self._url.auth,
+	    query: { 'start': self._n } })
+
+	    .then(function(response) {
+		self._n = response.headers['x-text-size'];
+
+		if (response.body.length > 0) {
+		    // If there is data, then push it and we are done
+		    return self.push(response.body);
+
+		} else if (response.headers['x-more-data'] !== 'true') {
+		    // If log if complete, then end the stream
+		    self.push(null);
+
+		} else if (response.body.length == 0) {
+		    // If there will be more data in the future,
+		    // then try to poll again in three seconds.
+		    setTimeout(get, self.pollInterval);
+		}
+	    })
+
+	    .catch(function(error) {
+		// ignore errors for now, we just keep polling
+		setTimeout(get, self.pollInterval);
+	    });
+    }
+
+    get();
+}
+
+module.exports = JenkinsOutput;
